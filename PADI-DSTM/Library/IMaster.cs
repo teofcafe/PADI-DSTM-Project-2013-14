4d57cf53145add2bd748dc451381701b8a625ee9
Telma Fernandes <telmix_27@hotmail.com>
2014-03-23 18:00:15 +0000

    commit que o chico pediu

--- PADI-DSTM/Library/IMaster.cs
+++ PADI-DSTM/Library/IMaster.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Transaction;
 
 namespace Library
 {
+
+    /*  Transaction Connect();
+        int RegisterServer(ip);
+        void UnregisterServer(int id);
+        notifyOvercharge(ind id);
+        URL GetServerURL(int id);
+        int GetServerWithObject(int uid);
+        void NotifyNeedMigrate(int id, int uid);
+     * 
+     * */
     interface IMaster
     {
+        //Transaction Connect();
+
+        int RegisterServer(string ip);
     }
 }
