460a1780ea869d35f0d8449db8d676b7148941e5
Jose Esteves <josemiguelce@gmail.com>
2014-03-24 17:51:53 +0000

    Implements Client

--- PADI-DSTM/Client/Client.cs
+++ PADI-DSTM/Client/Client.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
+using Library;
 
 namespace Client
 {
@@ -10,6 +17,23 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            // allocate and register channel
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel, true);
+            // get reference to remote service
+            IMaster master = (IMaster)Activator.GetObject(
+                typeof(IMaster),
+                "tcp://localhost:8089/Master");
+
+            try
+            {
+                Library.Transaction transaction = (Library.Transaction)master.Connect();
+                Console.WriteLine(transaction.ToString());
+            }
+            catch (SocketException)
+            {
+                System.Console.WriteLine("Could not locate server");
+            }
         }
     }
 }
---
+++ PADI-DSTM/Library/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Transaction
+    {
+        private Int32 timestamp;
+        private string ipCoordinator;
+
+        public Transaction(Int32 timestamp, string ipCoordinator)
+        {
+            this.timestamp = timestamp;
+            this.ipCoordinator = ipCoordinator;
+        }
+
+        public string ToString()
+        {
+            return "TimeStamp: " + timestamp.ToString() + ", Coordinator: " + ipCoordinator;
+        }
+    }
+}
