6c36832f1859f04cba4e21aeaf5a06b6267af776
Jose Esteves <josemiguelce@gmail.com>
2014-03-24 19:29:40 +0000

    PadInt

---
+++ PADI-DSTM/Library/TimeStamp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class TimeStamp
+    {
+        private long timestamp = DateTime.UtcNow.Ticks;
+
+        public long Timestamp
+        {
+            get { return this.timestamp; }
+            set { this.timestamp = value; }
+        }
+
+        public override string ToString()
+        {
+            return timestamp.ToString();
+        }
+
+        public static bool operator <(TimeStamp t1, TimeStamp t2)
+        {
+            return t1.timestamp<t2.timestamp;
+        }
+
+        public static bool operator >(TimeStamp t1, TimeStamp t2)
+        {
+            return t1.timestamp > t2.timestamp;
+        }
+
+        public static bool operator >=(TimeStamp t1, TimeStamp t2)
+        {
+            return t1.timestamp >= t2.timestamp;
+        }
+
+        public static bool operator <=(TimeStamp t1, TimeStamp t2)
+        {
+            return t1.timestamp <= t2.timestamp;
+        }
+    }
+}
---
+++ PADI-DSTM/Server/PadInt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using System.Collections;
+
+namespace Server
+{
+    class PadInt
+    {
+        private int value;
+        private int acessCounter = 0;
+
+        TimeStamp lastSuccessfulCommit;
+
+        public enum NextStateEnum {DELETE, MIGRATE, NONE};
+
+        private NextStateEnum nextState = NextStateEnum.NONE;
+
+        Hashtable trys = new Hashtable();
+
+        public int Read()
+        {
+            acessCounter++;
+            return this.value;
+        }
+
+        public int Read(TimeStamp timestamp)
+        {
+            acessCounter++;
+            return this.trys[timestamp];
+        }
+
+        public void Write(int value, TimeStamp timestamp)
+        {
+            acessCounter++;
+            trys[timestamp] = value;
+        }
+
+        public void Write(int value)
+        {
+            acessCounter++;
+            this.value = value;
+        }
+
+        public NextStateEnum NextState
+        {
+            get { return this.nextState; }
+            set { this.nextState = value; }
+        }
+    }
+}
